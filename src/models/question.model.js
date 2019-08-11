c9540452947c3eb5f6c584614320a9acb5fe13fb
hongkongaddoiladdoil <hongkongaddoiladdoil@gmail.com>
2019-08-11 08:31:52 -0700

    add question service

---
+++ src/models/question.model.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+
+// question-model.js - A KnexJS
+//
+// See http://knexjs.org/
+// for more of what you can do here.
+module.exports = function (app) {
+  const db = app.get('knexClient');
+  const tableName = 'question';
+  db.schema.hasTable(tableName).then(exists => {
+    if(!exists) {
+      db.schema.createTable(tableName, table => {
+        table.increments('id');
+        table.integer('user_id');
+        table.string('content');
+        table.timestamp('created_at').defaultTo(db.fn.now())
+      })
+        .then(() => console.log(`Created ${tableName} table`))
+        .catch(e => console.error(`Error creating ${tableName} table`, e));
+    }
+  });
+
+
+  return db;
+};
